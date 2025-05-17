c6b19bc665941bcffb0390ec9501d2e136168e6a
Al Mehedi <leftoverm9@gmail.com>
2025-05-17 10:31:51 +0600

    init 3

--- src/main.zig
+++ src/main.zig
@@ -8,14 +8,14 @@ const Executor = saturn.TaskExecutor(512);
 
 
 pub fn main() !void {
+    var gpa_mem = std.heap.DebugAllocator(.{}).init;
+    defer std.debug.assert(gpa_mem.deinit() == .ok);
+    // const heap = gpa_mem.allocator();
+
+
     try Signal.init();
     Signal.Linux.signal(linux.SIG.INT, Signal.register);
     Signal.Linux.signal(linux.SIG.TERM, Signal.register);
-
-    var gpa_mem = std.heap.DebugAllocator(.{}).init;
-    defer std.debug.assert(gpa_mem.deinit() == .ok);
-    const heap = gpa_mem.allocator();
-
 
     try Executor.init(null, true);
     defer Executor.deinit();
@@ -23,22 +23,8 @@ pub fn main() !void {
     try AsyncIo.init(true);
     defer AsyncIo.deinit();
 
-    const todo = try std.fs.cwd().openFile("./TODO.md", .{});
-    const buff = try heap.alloc(u8, 1024);
-    defer heap.free(buff);
-
-    try AsyncIo.read(res, null, .{
-        .fd = todo.handle, .buff = buff, .count = 1024, .offset = 0
-    });
-
-
     try AsyncIo.eventLoop();
 
     Executor.iso().condition.broadcast();
     Signal.terminate(@as(i64, Executor.iso().worker));
-}
-
-fn res(cqe_res: i32, userdata: ?*anyopaque) void {
-    _ = userdata;
-    std.debug.print("res: {}\n", .{cqe_res});
 }
